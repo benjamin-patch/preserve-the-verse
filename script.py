629efbbe6b9ba47d3864b46e23bd489ac1c9af73
Benjamin Patch <ben@benjaminpatch.com>
2025-03-07 18:07:00 -0800

    Initial commit of Python script.

---
+++ script.py
@@ -0,0 +1,4 @@
+# original data
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+print(highlighted_poems)
